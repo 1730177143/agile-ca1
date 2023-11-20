93165d65c33e62301c5fd4f76a56d2bca2045c5f
Long Liu <1730177143@qq.com>
2023-11-20 09:38:30 +0000

    test popular page

--- cypress/e2e/base2.cy.js
+++ cypress/e2e/base2.cy.js
@@ -62,3 +62,34 @@ describe("Trending", () => {
         });
     });
 });
+describe("Popular", () => {
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${Cypress.env(
+                "TMDB_KEY"
+            )}&language=en-US&include_adult=false&include_video=false&page=1`
+        )
+            .its("body")
+            .then((response) => {
+                movies = response.results;
+            });
+    });
+    beforeEach(() => {
+        cy.visit("/");
+        cy.get("button").contains("MoviesLists").click();
+        cy.contains( 'Popular').click();
+        cy.url().should("include", `/popular`);
+    });
+    describe("The popular Movies page", () => {
+        it("displays the page header and 20 movies", () => {
+            cy.get("h3").contains("Popular Movies");
+            cy.get(".MuiCardHeader-root").should("have.length", 20);
+        });
+
+        it("displays the correct movie titles", () => {
+            cy.get(".MuiCardHeader-content").each(($card, index) => {
+                cy.wrap($card).find("p").contains(movies[index].title);
+            });
+        });
+    });
+});

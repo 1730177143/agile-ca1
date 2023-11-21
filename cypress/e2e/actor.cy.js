7d9c52a39195ee4c278f8ada8d8e498e718503ed
Long Liu <1730177143@qq.com>
2023-11-21 22:21:32 +0000

    fix and test actor details page

---
+++ cypress/e2e/actor.cy.js
@@ -0,0 +1,56 @@
+let actors;
+let actor;
+describe("Actor Home", () => {
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env(
+                "TMDB_KEY"
+            )}&language=en-US&include_adult=false&include_video=false&page=1`
+        )
+            .its("body")
+            .then((response) => {
+                actors = response.results;
+            });
+    });
+    beforeEach(() => {
+        cy.visit("/");
+        cy.get(".MuiButtonBase-root").contains("ActorHome").click();
+        cy.url().should("include", `/actors`);
+    });
+    describe("The popular actors page", () => {
+        it("displays the page header and 20 actors", () => {
+            cy.get("h3").contains("Discover Actors");
+            cy.get(".MuiCardHeader-root").should("have.length", 20);
+        });
+
+        it("displays the correct actors name", () => {
+            cy.get(".MuiCardHeader-content").each(($card, index) => {
+                cy.wrap($card).find("p").contains(actors[index].name);
+            });
+        });
+    });
+    describe("The Actor Details page", () => {
+        before(() => {
+            cy.request(
+                `https://api.themoviedb.org/3/person/${
+                    actors[0].id
+                }?api_key=${Cypress.env("TMDB_KEY")}`
+            )
+                .its("body")
+                .then((actorDetails) => {
+                    actor = actorDetails;
+                });
+        });
+        beforeEach(() => {
+            cy.visit(`/actors/${actors[0].id}`);
+        });
+        it(" displays the actor name, biography and birthday and place of birth", () => {
+            cy.get("h3").contains(actor.name);
+            cy.get("h3").get(".MuiTypography-root").contains("Biography");
+            cy.get(".MuiTypography-root").next().contains(actor.biography);
+            cy.get(".MuiChip-root").contains(actor.birthday);
+            cy.get(".MuiChip-root").contains(actor.place_of_birth.toLocaleString());
+            cy.get(".MuiChip-root").contains(actor.known_for_department);
+        });
+    });
+});

a5b6a37e9dee38b7422b839f4853268f218e5775
Long Liu <1730177143@qq.com>
2023-11-21 22:58:43 +0000

    test movie credits page

---
+++ cypress/e2e/credits.cy.js
@@ -0,0 +1,40 @@
+let actors;
+let actor;
+let movies;
+let movie;
+let id;
+describe("Movie credits", () => {
+    before(() => {
+        cy.request(`https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=1`)
+            .its("body")
+            .then((response) => {
+                actors = response.results;
+                cy.visit("/");
+                cy.get(".MuiButtonBase-root").contains("ActorHome").click();
+                cy.url().should("include", `/actors`);
+                cy.get("button").contains("More Info ...").click();
+                cy.get(".MuiFab-root").contains("relational movies").click();
+                cy.url().should("include", `/movieCredits`);
+                id = actors[0].id;
+                cy.request(`https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${Cypress.env("TMDB_KEY")}`)
+                    .its("body")
+                    .then((response) => {
+                        movies = response.cast;
+                    });
+            });
+    });
+    beforeEach(() => {
+        cy.visit(`/movieCredits/${id}`);
+    });
+    describe("The Movie credits page", () => {
+        it("displays the page header and movies", () => {
+            cy.get("h3").contains("Movie Credits");
+        });
+
+        it("displays the correct movies name", () => {
+            cy.get(".MuiCardHeader-content").each(($card, index) => {
+                cy.wrap($card).find("p").contains(movies[index].title);
+            });
+        });
+    });
+});
